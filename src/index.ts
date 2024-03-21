d604c705cfdda0646c0c5532a86044a9261c75e2
youssef moussaid <144618017+yoyo58@users.noreply.github.com>
2024-03-21 12:04:22 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -4,11 +4,11 @@ import { clients } from "./entity/clients"; // Assumant que la classe a été re
 
 AppDataSource.initialize().then(async () => {
     console.log("Inserting a new user into the database...");
-    const user = new clients(); // Utilisation de 'Client' si la classe a été renommée
+    const user = new clients(); 
     user.nom = "Timber";
     user.prenom = "Saw";
     user.email = "youssef@gmail.com";
-    user.telephone = "1234567890"; // Fournir une valeur valide pour le téléphone
+    user.telephone = "1234567890"; 
     user.adresse = "mondelange 57300";
     user.dateCreation = new Date('2024-02-17T01:00:00.000Z');
     user.dateMiseajour = new Date('2024-03-17T01:00:00.000Z');
